24fbf27663eaf2cd80a09bca7b0b2f3a144af7bc
arthur <arthur.oceanus@gmail.com>
2025-03-08 15:13:13 +0800

    feat: setup vite server port

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from "node:path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.VITE_PORT ? +process.env.VITE_PORT : 3000,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
